9e2b31958cb3775f1ea90a173f1f99197b38c2dc
yigitalpkaraca <yigitalpkaraca@posta.mu.edu.tr>
2025-03-06 13:17:00 +0300

    week3 uploaded

--- week3/src/FindGrade.java
+++ week3/src/FindGrade.java
@@ -1,6 +1,33 @@
 public class FindGrade{
 
-    public static void main(String[] args){
+    public static void main(String[] args) {
+        int lenght_of_args = args.length;
+        if (lenght_of_args == 0) {
+        System.out.println("You should give em a score!!!");
+    }
+        else {
+        int grade = Integer.parseInt(args[0]);
+
+            if(grade >= 90){
+                System.out.println("Grade : A");
+
+
+            }    else if(grade >= 80 && grade < 89) {
+            System.out.println("Grade : B ");
+
+        }   else if(grade >= 70 && grade< 79){
+                System.out.println("Grade : C ");
+            }
+            else if(grade >= 60 && grade < 69){
+                System.out.println("Grade : D");
+
+            } else if(grade >= 00 && grade < 59){
+                System.out.println("Grade : FF");
+
+            }else {
+                System.out.println("invalid score");
+            }
+        }
 
         //Get the score from the user as a file parameter
         //Calculate the grade based on the score
@@ -18,5 +45,48 @@ public class FindGrade{
     }
 
 
+    public static class FindMinimum {
+        public static void main(String[] args) {
+            int num1 = Integer.parseInt(args[0]);
+            int num2 = Integer.parseInt(args[1]);
+            int num3 = Integer.parseInt(args[2]);
+                /*if (num1 < num2 && num1 < num3){
+                    System.out.println("Lowest number is num 1 =" + num1);
+                }   else if (num2 < num3 && num2 < num3){
+                    System.out.println("Lowest is number 2 = "+num2);
 
+                }else {
+                    System.out.println("Lowest is number 3 =" +num3);*/
+           /*     }*/
+            boolean min_num1 = num1 < num2 && num1 < num3;
+            boolean min_num2 = num2 < num1 && num2 < num3;
+            int minimum = min_num1 ? num1:
+                                min_num2 ? num2: num3;
+            System.out.println(minimum);
+
+
+        }
+    }
+
+    public static class FindPrimes {
+        public static void main(String[] args) {
+            int number = Integer.parseInt(args[0]);
+
+            for (int i = 2; i <= number; i++) {
+                boolean isPrime = true;
+                for (int j = 2; j < i; j++) {
+                    if (i % j == 0) {
+                        isPrime = false;
+                        break;
+                    }
+
+                }
+                if (isPrime) {
+                    System.out.println(i);
+
+                }
+            }
+
+        }
+    }
 }
---
+++ week3/src/FindMaximum.java
@@ -0,0 +1,25 @@
+public class FindMaximum {
+    public static void main (String[] args){
+    /*    if(args.length <2){
+            return;
+        }
+        int a1 = Integer.parseInt(args[0]);
+        int a2 = Integer.parseInt(args[1]);
+
+        if (a1 < a2) {
+            System.out.println("Maximum is"  +a1);
+        } else{
+            System.out.println("Maximum is"  +a2);*/
+/*
+
+        }
+    }
+}
+}*/
+    int num1 = Integer.parseInt(args[0]);
+    int num2 = Integer.parseInt(args[1]);
+
+    boolean condiiton = num1 > num2;
+    int greatest = condition ? num
+
+        }
